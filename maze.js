e9e4f1ac69fa07d42f0ddf20607a064d33951c1d
Daniel <danieldormer87@gmail.com>
2018-10-13 09:10:03 -0500

    win message

--- maze.js
+++ maze.js
@@ -1,17 +1,23 @@
 // JavaScript Document
 window.onload = go;
-
+var nohit = true;
 function go(){
 	var bs = document.querySelectorAll(".boundary");
 	for(var x= 0; x < bs.length; x++){
 		bs[x].addEventListener("mouseover", modify);
 	}
-	
+	document.getElementById("end").addEventListener("mouseover", win);	
 }
 function modify(){
 	var bs = document.getElementsByClassName("boundary");
 	for( var x = 0; x < bs.length; x++){
 		bs[x].classList.add("youlose");
+	}
+	nohit = false;
+}
+function win(){
+	if (nohit === true){
+		alert("You win!");
 	}
 }
 	
