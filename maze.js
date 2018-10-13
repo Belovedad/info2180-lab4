7e732216f082fed3b5ae1d5462a3bda186ec18a5
Daniel <danieldormer87@gmail.com>
2018-10-13 09:03:07 -0500

    all boundaries

--- maze.js
+++ maze.js
@@ -2,5 +2,16 @@
 window.onload = go;
 
 function go(){
-	document.getElementById("boundary1").addEventListener("mouseover","youlose");
+	var bs = document.querySelectorAll(".boundary");
+	for(var x= 0; x < bs.length; x++){
+		bs[x].addEventListener("mouseover", modify);
+	}
+	
 }
+function modify(){
+	var bs = document.getElementsByClassName("boundary");
+	for( var x = 0; x < bs.length; x++){
+		bs[x].classList.add("youlose");
+	}
+}
+	
